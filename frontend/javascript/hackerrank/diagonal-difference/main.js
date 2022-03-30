aa44f0221fc6950c6f9cdaf03844abf2d64b7d33
Amario Fausta <amarioharlastputra@gmail.com>
2022-03-30 23:21:36 +0700

    diagonal

--- frontend/javascript/hackerrank/diagonal-difference/main.js
+++ frontend/javascript/hackerrank/diagonal-difference/main.js
@@ -12,8 +12,17 @@
 // Full Problem: https://www.hackerrank.com/challenges/diagonal-difference/problem
 
 function diagonalDifference(arr) {
-  // Write your code here
-  // TODO: answer here
+  let a = 0;
+  let b = 0;
+
+  for(let i = 0; i < arr.length; i ++){
+    a += arr[i][i]
+  }
+  for(let i = 0; i < arr.length; i ++){
+    b += arr[2-i][i]
+  }
+
+  return Math.abs(a-b)
 }
 
 function main() {
