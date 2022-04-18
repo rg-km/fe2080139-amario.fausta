7b90d129582d04131984cedb4ab19874f21c8048
Amario Fausta <amarioharlastputra@gmail.com>
2022-04-18 11:47:30 +0700

    dfa 2

--- frontend/es6/5-destructuring-firstname-assignment/index.js
+++ frontend/es6/5-destructuring-firstname-assignment/index.js
@@ -1,9 +1,12 @@
 /** Akses user name, namun dengan nama variabel yang berbeda dari atribut pada objek user
  */
-const getUserFirstName = (user) => {
+ const getUserFirstName = (user) => {
   // TODO: answer here
+  const { name: firstName } = user;
+  return firstName;
 };
 
 console.log(getUserFirstName({ name: "John", email: "john@example.com" }));
+
 
 module.exports = getUserFirstName
