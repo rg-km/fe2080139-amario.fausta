a7f52502288dc9b7161de9e3f57b38e2c2a515b6
Amario Fausta <amarioharlastputra@gmail.com>
2022-04-19 16:35:21 +0700

    callback-cp

--- frontend/javascript/09-callback-cp/index.js
+++ frontend/javascript/09-callback-cp/index.js
@@ -4,11 +4,27 @@
 // contoh: hehe => hheehhee
 function doubleChars(str) {
   // TODO: answer here
-}
+  let a = str.split("")
+  let c = []
+  for (let i = 0; i < str.length; i ++){
+    c.push(a[i])
+    c.push(a[i])
+  }
+  return c
+}  
 
 // function ini mengulang pengaplikasian callback pada str sejumlah num
 function repeat(str, num, cb) {
   // TODO: answer here
+  let b = []
+  let d = cb(str) //double
+  for (let k = 0; k < d.length; k ++){
+    for (let j = 0; j < num; j ++){
+      b.push(d[k])
+    }
+  }
+  b = b.join("")
+  return b
 }
 
 console.log(repeat("triple", 2, doubleChars)); //  ttttrrrriiiipppplllleeee
