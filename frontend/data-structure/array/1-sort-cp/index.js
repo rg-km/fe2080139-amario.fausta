3f76eabf1d802c6e5a37f02c0fce741e9bb43d54
Amario Fausta <amarioharlastputra@gmail.com>
2022-04-21 14:15:40 +0700

    sort-cp

--- frontend/data-structure/array/1-sort-cp/index.js
+++ frontend/data-structure/array/1-sort-cp/index.js
@@ -13,10 +13,26 @@
 // [1,2,3,4,5] -> [2,3,4,5,1] -> [3,4,5,1,2] -> [4,5,1,2,3] -> [5,1,2,3,4]
 
 function sort(arr) {
+    for(let i = 0; i < arr.length; i++ ){
+        for(let j = i + 1; j < arr.length; j ++){
+            if (arr[i] > arr[j]){
+                swap = arr[i]
+                arr[i] = arr[j]
+                arr[j] = swap
+            }
+        } 
+    }
     return arr // TODO: replace this
 }
 
 function rotateLeft(d, arr) {
+    for(let k =0; k < d; k ++){
+        for(let i = 0; i < 4; i++ ){
+            swap = arr[i]
+            arr[i] = arr[i+1]
+            arr[i+1] = swap       
+        }
+    }
     return arr // TODO: replace this
 }
 
