426700e70eaf903f19268ba80d52d6d2f628c057
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-04-27 16:38:46 +0700

    tanda kurung

--- frontend/data-structure/assignment/text-editor/index.js
+++ frontend/data-structure/assignment/text-editor/index.js
@@ -1,21 +1,35 @@
 module.exports = class TextEditor {
     constructor() {
         // TODO: answer here
+        this.data = []
+        this.top = -1
+        this.memory = []
     }
 
     write(c) {
         // TODO: answer here
+        this.top += 1
+        return this.data.push(c)
     }
 
     read() {
         // TODO: answer here
+        let ayo = this.data.join("")
+        return ayo
     }
 
     undo() {
+        this.memory.push(this.data[this.top])
+        this.top -= 1
         // TODO: answer here
+        return this.data.pop()
     }
     
     redo() {
         // TODO: answer here
+        this.top += 1
+        let bisa = this.memory[this.memory.length - 1]
+        this.memory.pop()
+        return this.data.push(bisa)
     }
 }
