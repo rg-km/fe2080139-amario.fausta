2c7d92c3c65f47fd470824374d66d1785b513908
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-04 12:51:34 +0700

    let result

--- frontend/data-structure/recursion/1-recursion-cp/index.js
+++ frontend/data-structure/recursion/1-recursion-cp/index.js
@@ -21,6 +21,7 @@ function myPow(x, n) {
     
     // TODO: answer here
     // TODO: answer here
+    let result = pow%2===0 ? myPow(x*x,pow/2) : myPow(x*x,(pow-1)/2) * x;
 }
 
 module.exports = { myPow }
