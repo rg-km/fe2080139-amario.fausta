006c6792476fa823f0d9f2c65541debd0c40607b
Amario Fausta <75331373+Amario1306619051@users.noreply.github.com>
2022-05-22 18:24:42 +0700

    mencoba upload

--- frontend/react/react-project/src/App.js
+++ frontend/react/react-project/src/App.js
@@ -1,8 +1,33 @@
 import "./App.css"
 // TODO: answer here
 
+import Home from "./routes/Home";
+import Profile from "./routes/Profile";
+import Navbar from "./components/Navbar"
+import PostCard from "./components/PostCard"
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   // TODO: answer here
+  return (
+    <div aria-label="App" className="App">
+      <h1 aria-label="App Title">IG Clone</h1>
+      
+      <div className="ImportItem">
+          <Navbar />
+          <PostCard />
+      </div>
+
+      <div className="routes" aria-label="routes">
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="Profile" element={<Profile />} />
+      </Routes>
+
+    </div>
+  </div>
+  );
 }
 
 export default App
